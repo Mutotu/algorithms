f1f6c3b847e74ef486b4c4523afedd65db1c973c
mutotu <muttalip.aydemir@gmail.com>
2022-03-22 11:36:57 +0000

    trees

--- index.js
+++ index.js
@@ -529,12 +529,12 @@ class DoublyLinkedList {
 ////////STACKCS
 //
 /////Class Node
-class Node {
-  constructor(value) {
-    this.value = value;
-    this.next = null;
-  }
-}
+// class Node {
+//   constructor(value) {
+//     this.value = value;
+//     this.next = null;
+//   }
+// }
 class Stack {
   constructor(value) {
     const newNode = new Node(value);
@@ -600,3 +600,65 @@ class Queue {
   }
 }
 // let myQueue = new Queue(9)
+////////TREES//////
+// O(log n)
+//divide and conquer
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BST {
+  constructor() {
+    this.root = null;
+  }
+
+  insert(value) {
+    const newNode = new Node(value);
+    if (root === null) {
+      this.root = newNode;
+      return this;
+    }
+    let temp = this.root;
+    while (true) {
+      if (newNode.value === temp.value) return undefined;
+      if (newNode.value < temp.value) {
+        if (temp.left === null) {
+          temp.left = newNode;
+          return this;
+        }
+        temp = temp.left;
+      } else {
+        if (temp.right === null) {
+          temp.right = newNode;
+          return this;
+        }
+        temp = temp.right;
+      }
+    }
+  }
+  contains(value) {
+    if (this.root === null) return false;
+    let temp = this.root;
+    while (temp) {
+      if (value < temp.value) {
+        temp = temp.left;
+      } else if (value > temp.value) {
+        temp = temp.right;
+      } else {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  minValueNode(currentNode) {
+    while (!currentNode.left) {
+      currentNode = currentNode.left;
+    }
+    return currentNode;
+  }
+}
